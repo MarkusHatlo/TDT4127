3810c734020cb500e4f3e74793fe2ec4a24f8002
Markus Hatlo <marhat182@gmail.com>
2024-12-15 13:39:52 +0100

    holder pa R2023

---
+++ Eksamensoving/eksamenR2023.py
@@ -0,0 +1,34 @@
+# fruits = ["apple", "banana", "cherry", "date"]
+# fruits[2] = "grape"
+
+# print(fruits[2])
+
+# total = 0
+# for num in range(1, 6):
+#     total += num
+
+# print(total)
+
+# x = "10"
+# y = 2.5
+# z = int(x) + y
+
+# print(z)
+
+# numbers = [1, 2, 3, 4, 5]
+# sliced = numbers[1:4]
+
+# print(sliced)
+
+# for i in range(5):
+#     if i == 3:
+#         break
+#     else:
+#         print("Done")
+
+x_value = 1.0e10
+increment= 1.23e-10
+for i in range(20):
+    x_value = x_value + increment
+
+print(x_value)
